2345c5ce6a85e0fd80d9fe9fbe4d1c9c7386d55f
chauseth <alexandre.chiquet@hotmail.com>
2020-09-30 15:21:38 +0200

    Ajout programme stairway, tournant 100 fois automatiquement

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,21 +1,12 @@
-def initialisation 
-    etage_joueur = 1
-    nb_etage = 10
-    return etage_joueur, nb_etage
-end
-
-def check_etage (etage_joueur, etage)
-    if etage_joueur >= etage 
-        puts "Felicitation, vous avez gagné !"
-        exit
+def check_etage (etage_joueur, etage, nb_tour)
+    case check
+    when etage_joueur <= 0
+        etage_joueur = 0
+        return etage_joueur
+    when etage_joueur >= 10
+        return etage_joueur, nb_tour, win
     else
-        if etage_joueur <= 0
-            etage_joueur = 0
-            puts "Il faut tout remonter !"
-        else
-            puts "Vous êtes à l'étage #{etage_joueur}/#{etage}"
-        end
-    end
+end
     
 end
 
@@ -44,58 +35,61 @@ end
 
 def dice
     lancer = 0
-    puts "Voulez-vous lancer les dés ? (oui/non)"
-    print "> "
-    choice = gets.chomp
-    if choice == "non"
-        return false
-    else
-       if choice == "oui"
-           number = rand(1..6)
-           puts "Vous avez fait un ...  #{number}!"
-           return number
-       else
-           puts "Merci d'indiquer oui ou non"
-       end 
-    end
+    number = rand(1..6)
+    return number
 end
 
 def operation (jet, etage_joueur)
+    
    case jet
         when 1
             etage_joueur -= 1
-            puts "Aïe, vous êtes descendu d'une marche, vous êtes maintenant a la marche #{etage_joueur}"
             return etage_joueur
         when 2..4
-            puts "Vous ne bougez pas"
             return etage_joueur
         when 5..6
             etage_joueur += 1
-            puts "Bravo, vous avez avancé d'une marche ! Vous êtes maintenant a la marche #{etage_joueur}"
             return etage_joueur
         else
             puts "Erreur : Mauvaise entrée"  
-   end
+        end
 end
 
-def start
-    initialisation
-
-    perform
+def initialisation 
+    etage_joueur = 1
+    nb_etage = 10
+    return etage_joueur, nb_etage
 end
 
 def perform
     etage_joueur = initialisation[0]
     etage = initialisation[1]
-    while etage_joueur <= etage
-
-        escalier(etage, etage_joueur)
-        check_etage(etage_joueur, etage)
+    nb_tour = 0
+    while etage_joueur < etage
+        #escalier(etage, etage_joueur)
         lancer = dice.to_i
         etage_joueur = operation(lancer, etage_joueur)
-        puts etage_joueur
+        nb_tour +=1
     end
+    tour_final = nb_tour
+    return tour_final
+    
 
 end
 
-start
+def start
+
+    perform
+end
+
+def average_finish_time
+    avg_turn = []
+    100.times { 
+        avg_turn.push(perform)
+    }
+    puts "Résumé des tours : #{avg_turn}"
+    puts " LA MOYENNE EST : #{avg_turn.inject(&:+) / avg_turn.size}"
+#{avg_turn.inject(&:+) / avg_turn.size}
+end
+
+average_finish_time
